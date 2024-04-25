6999bf25cf705ebb2125edf53e445edf55b830d5
c00lb34ns <philippe_bral@hotmail.com>
2024-04-25 12:11:13 +0200

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,11 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+	$('.collapsible').collapsible();
+	$('.scrollspy').scrollSpy();
+	
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
